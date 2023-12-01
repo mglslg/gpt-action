31ae5f4347c7e1f585b4337b7b960497f4b3c848
01420524 <suolongga@sf-express.com>
2023-12-01 18:30:19 +0800

    init

---
+++ src/main/java/com/hogwartscoder/gptaction/rest/SystemController.java
@@ -0,0 +1,12 @@
+package com.hogwartscoder.gptaction.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SystemController {
+    @GetMapping("/health/check")
+    public String getRuleGroupPage() {
+        return "ok";
+    }
+}
