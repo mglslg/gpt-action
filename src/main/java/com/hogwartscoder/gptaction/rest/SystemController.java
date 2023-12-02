9944196a750a2c719b4be66771e52df67a74e278
suolongga <mglslg@qq.com>
2023-12-02 12:37:24 +0800

    8080

--- src/main/java/com/hogwartscoder/gptaction/rest/SystemController.java
+++ src/main/java/com/hogwartscoder/gptaction/rest/SystemController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SystemController {
     @GetMapping("/health/check")
-    public String getRuleGroupPage() {
+    public String healthcheck() {
         return "ok\n";
     }
 }
